9751ac658a11cf4164d4a670c48f347938458614
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-07-17 23:25:03 +0900

    poomsae/poomsae.cs -> poomsae/App.cs

--- poomsae/App.cs
+++ poomsae/App.cs
@@ -1,15 +1,25 @@
-﻿using System;
-
-using Xamarin.Forms;
-
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="MyMasterDetailPage.cs" company="shinriyo">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Poomsae
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Xamarin.Forms;
+
+    /// <summary>
+    /// App.
+    /// </summary>
     public class App : Application
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Poomsae.App"/> class.
+        /// </summary>
         public App()
         {
             // The root page of your application
@@ -47,20 +57,31 @@ namespace Poomsae
             //			};
         }
 
+        /// <summary>
+        /// Ons the start.
+        /// </summary>
+        /// <returns>The start.</returns>
         protected override void OnStart()
         {
             // Handle when your app starts
         }
 
+        /// <summary>
+        /// Ons the sleep.
+        /// </summary>
+        /// <returns>The sleep.</returns>
         protected override void OnSleep()
         {
             // Handle when your app sleeps
         }
 
+        /// <summary>
+        /// Ons the resume.
+        /// </summary>
+        /// <returns>The resume.</returns>
         protected override void OnResume()
         {
             // Handle when your app resumes
         }
     }
 }
-
