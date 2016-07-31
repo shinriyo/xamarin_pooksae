652c071e0b63cf5a69827383e056281e444da868
shinriyo <shinriyo@shinriyos-MacBook.local>
2016-08-01 00:35:35 +0900

    PoomsaeMode準備

---
+++ poomsae/Scripts/Models/PoomsaeModel.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PoomsaeModel.cs" company="shinriyo">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Poomsae
+{
+    using Realms;
+    using Realms.Tool;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Poomsae model.
+    /// </summary>
+    public class PoomsaeModel : RealmObject, IModel
+    {
+        public enum ArtType
+        {
+            Punch,
+            Kick,
+            Chop,
+            Guard,
+        }
+
+        [ObjectId]
+        public string Id { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset Updated { get; set; }
+
+        /// <summary>
+        /// ローカライズの言語.
+        /// </summary>
+        /// <value>The country identifier.</value>
+        public string Language { get; set; }
+
+        /// <summary>
+        /// 0:パンチ or 1:キック or 2:手刀 or 3:受け.
+        /// </summary>
+        /// <value>The key.</value>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// 級/段.
+        /// </summary>
+        /// <value>The kyu.</value>
+        public int Kyu { get; set; }
+
+        /// <summary>
+        /// 実際の文字列の値.
+        /// </summary>
+        /// <value>The value.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 説明.
+        /// </summary>
+        /// <value>The value.</value>
+        public string Desc { get; set; }
+
+        /// <summary>
+        /// 詳細.
+        /// </summary>
+        /// <value>The value.</value>
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// 画像.
+        /// </summary>
+        /// <value>The value.</value>
+        public string Picture { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[ArtModel: Id={0}, Created={1}, Updated={2}, Country={3}, Key={4}, Name={5}, Desc={6}, Detail={7}, Picture={8}]", Id, Created, Updated, Language, Type, Name, Desc, Detail, Picture);
+        }
+    }
+}
+
--- poomsae/Scripts/Pages/PoomsaeDetailPages/PoomsaeDetailPageBase.cs
+++ poomsae/Scripts/Pages/PoomsaeDetailPages/PoomsaeDetailPageBase.cs
@@ -8,12 +8,19 @@ namespace Poomsae
 {
     using System;
     using System.Collections.ObjectModel;
+    using System.Linq;
     using System.Windows.Input;
+    using Realms.Tool;
     using VideoPlayerSample.Services;
     using Xamarin.Forms;
 
+    /// <summary>
+    /// Poomsae detail page base.
+    /// </summary>
     public class PoomsaeDetailPageBase : ContentPage
     {
+        private const int initKyu = -1;
+
         /// <summary>
         /// Data.
         /// </summary>
@@ -35,6 +42,93 @@ namespace Poomsae
             {
                 Title = title;
             }
+        }
+
+        /// <summary>
+        /// Creates the groups.
+        /// </summary>
+        /// <returns>The groups.</returns>
+        /// <param name="title">Title.</param>
+        /// <param name="order">Order.</param>
+        /// <param name="image">Image.</param>
+        protected ObservableCollection<Group> CreateGroups(int type, Action<string, string, string> action)
+        {
+            var groups = new ObservableCollection<Group>();
+            // TODO:
+            var kyuOrDan = (type == 0) ? "級" : "段";
+            string iconImage = "kick_icon.png";
+            string detailImageBase = @"poomsae.Resources.Punch.{0}.jpg";
+
+            var poomsaeModelController = new Controller<PoomsaeModel>();
+            var res = poomsaeModelController.GetResults().Where(d => d.Type == type);
+            Group group = null;
+
+            // 判定用.
+            int nowKyu = initKyu;
+
+            foreach (var item in res)
+            {
+                var kyu = item.Kyu;
+                var name = item.Name;
+                var desc = item.Desc;
+                var detail = item.Detail;
+                var picture = item.Picture;
+                System.Diagnostics.Debug.WriteLine("{0} {1} {2} {3} {4}",
+                                                   kyu, name, desc, detail, picture);
+                // 変わった時かつ最初ではない前ループのグループを追加.
+                if (nowKyu != kyu && nowKyu != initKyu)
+                {
+                    groups.Add(group);
+                }
+
+                // 変わった時または最初.
+                if (nowKyu != kyu || nowKyu == initKyu)
+                {
+                    // N級.
+                    group = new Group(string.Format("{0}{1}", kyu, kyuOrDan));
+                }
+
+                var data = CreateData(name, desc, detail, iconImage,
+                                      string.Format(detailImageBase, picture), action);
+                group.Add(data);
+                nowKyu = kyu; // 更新.
+            }
+
+            // 1つでもあれば最後に追加.
+            if (res.Count() > 0)
+            {
+                groups.Add(group);
+            }
+
+            return groups;
+        }
+
+        /// <summary>
+        /// TODO: Creates the data.
+        /// </summary>
+        /// <returns>The data.</returns>
+        /// <param name="name">Name.</param>
+        /// <param name="desc">Desc.</param>
+        /// <param name="detail">Detail.</param>
+        /// <param name="imageName">Image name.</param>
+        /// <param name="image">Image.</param>
+        /// <param name="action">Action.</param>
+        private static Data CreateData(
+            string name, string desc, string detail, string imageName,
+            string image,
+            Action<string, string, string> action)
+        {
+            var data = new Data
+            {
+                Name = name,
+                Description = desc,
+                Picture = imageName,
+                OnClick = new Command(() =>
+                {
+                    action(name, detail, image);
+                })
+            };
+            return data;
         }
 
         /// <summary>
